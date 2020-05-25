98c692a1bdbac129a6fabc75e5269e45b1caffe1
LAPTOP-1821QFF9\krack <kracker86@interia.pl>
2020-05-25 10:40:16 +0200

    step 4

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express=require('express');
 const app=express();
 
-app.listen(8000,function(){
-    console.log("server is running");
+const mockUserdDta=[
+{name:'Mark'},
+{name:'Jill'}]
+app.get('/users',function(reqest,respons){
+
+    respons.json({
+                   success :true,
+                   message: 'successfully got users. Nice!',
+ 	 	           users: mockUserData
+
+
+    })
 })
+app.listen(8000,function(){console.log('server is listening')})
